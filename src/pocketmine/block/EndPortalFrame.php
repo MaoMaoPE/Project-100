7d4272580037804319db1019a99939c378a8ad11
冷例 <162157289+rpg636zjhi@users.noreply.github.com>
2026-07-13 13:12:26 +0800

    修复EndPortaFrame 的转向问题
    修复End Porta Frame 的转向问题

--- src/pocketmine/block/EndPortalFrame.php
+++ src/pocketmine/block/EndPortalFrame.php
@@ -1,66 +1,94 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
- *
+ * 
+ *  ____                     _                 _             _    ___     ___  
+ * |  _ \   _ __    ___     (_)   ___    ___  | |_          / |  / _ \   / _ \ 
+ * | |_) | | '__|  / _ \    | |  / _ \  / __| | __|  _____  | | | | | | | | | |
+ * |  __/  | |    | (_) |   | | |  __/ | (__  | |_  |_____| | | | |_| | | |_| |
+ * |_|     |_|     \___/   _/ |  \___|  \___|  \__|         |_|  \___/   \___/ 
+ *                        |__/                                                 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- * 
+ * @author Project-100
+ * @link https://github.com/MaoMaoPE/Project-100
  *
+ * 
 */
 
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
 use pocketmine\math\AxisAlignedBB;
+use pocketmine\Player;
 
-class EndPortalFrame extends Solid implements SolidLight{
+class EndPortalFrame extends Solid implements SolidLight {
 
-	protected $id = self::END_PORTAL_FRAME;
+    protected $id = self::END_PORTAL_FRAME;
 
-	public function __construct($meta = 0){
-		$this->meta = $meta;
-	}
+    public function __construct($meta = 0){
+        $this->meta = $meta & 0x03;
+    }
 
-	public function getLightLevel(){
-		return 1;
-	}
+    public function getLightLevel(){
+        return 1;
+    }
 
-	public function getName() : string{
-		return "End Portal Frame";
-	}
+    public function getName() : string{
+        return "End Portal Frame";
+    }
 
-	public function getHardness() {
-		return -1;
-	}
+    public function getHardness(){
+        return -1;
+    }
 
-	public function getResistance(){
-		return 18000000;
-	}
+    public function getResistance(){
+        return 18000000;
+    }
 
-	public function isBreakable(Item $item){
-		return false;
-	}
+    public function isBreakable(Item $item){
+        return false;
+    }
 
-	protected function recalculateBoundingBox() {
+    protected function recalculateBoundingBox(){
+        return new AxisAlignedBB(
+            $this->x,
+            $this->y,
+            $this->z,
+            $this->x + 1,
+            $this->y + 0.8125,
+            $this->z + 1
+        );
+    }
 
-		return new AxisAlignedBB(
-			$this->x,
-			$this->y,
-			$this->z,
-			$this->x + 1,
-			$this->y + (($this->getDamage() & 0x04) > 0 ? 1 : 0.8125),
-			$this->z + 1
-		);
-	}
+    public function getDirection(){
+        return $this->meta & 0x03;
+    }
+
+    public function setDirection($direction){
+        $this->meta = ($this->meta & ~0x03) | ($direction & 0x03);
+    }
+
+    public function canBeActivated() : bool{
+        return false;
+    }
+
+    public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+        if($player instanceof Player){
+            $this->setDirection(($player->getDirection() + 3) & 0x03);
+        }
+        $this->getLevel()->setBlock($block, $this, true, true);
+        return true;
+    }
+
+    public function onActivate(Item $item, Player $player = null){
+        return false;
+    }
+
+    public function getDrops(Item $item) : array{
+        return [];
+    }
 }
