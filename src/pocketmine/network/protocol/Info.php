d42e597cc089a64630a180c47d01fcf56da6e3e7
XinYueNeko <3649745663@qq.com>
2026-07-21 19:08:53 +0800

    牙膏

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -45,7 +45,7 @@ interface Info{
 	 * 以上协议均对应正式版版本号
 	 */
 	const MINECRAFT_VERSION = ["v1.0.0","v1.0.3","v1.0.4","v1.0.5","v1.0.6","v1.0.7"/*,"1.1.0"*/];
-	const MINECRAFT_VERSION_NETWORK = "1.0";
+	const MINECRAFT_VERSION_NETWORK = "1.0.0";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
