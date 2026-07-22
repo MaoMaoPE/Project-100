a683a82d43e64805b33cd2dca376d4a37b351df3
XinYueNeko <3649745663@qq.com>
2026-07-23 07:28:56 +0800

    回滚 Chicken.php

--- src/pocketmine/entity/Chicken.php
+++ src/pocketmine/entity/Chicken.php
@@ -24,7 +24,6 @@ namespace pocketmine\entity;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\Item as ItemItem;
-use pocketmine\math\Vector3;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
 
@@ -36,12 +35,11 @@ class Chicken extends Animal{
 	public $height = 0.6;
 	public $speed = 1.0;
 
-	private $moveDirection = null; //移动方向
-	private $tempTicker = 0;
-	private $tempTicking = false;
-	private $moveTicker = 0; //运动计时器
-
 	public $dropExp = [1, 3];
+
+	public function getSpeed(): float {
+		return $this->speed;
+	}
 	
 	public function getName() : string{
 		return "Chicken";
@@ -67,39 +65,6 @@ class Chicken extends Animal{
 		$pk->metadata = $this->dataProperties;
 		$player->dataPacket($pk);
 		parent::spawnTo($player);
-	}
-
-	private function generateRandomDirection(){ //运动
-		return new Vector3(mt_rand(-1000, 1000) / 1000, 0, mt_rand(-1000, 1000) / 1000);
-	}
-
-	private function getVelY(){
-		$expectedPos = (new Vector3($this->x + $this->moveDirection->x * $this->speed, $this->y + $this->motionY, $this->z + $this->moveDirection->z * $this->moveSpeed))->round();
-		$block0 = $this->getLevel()->getBlock($expectedPos);
-		$block1 = $this->getLevel()->getBlock($expectedPos->add(0, 1, 0));
-		if($block1->getId() != 0) return 1.2;
-		return 0;
-	}
-
-	public function onUpdate($tick) { //TODO 鸡的AI
-		if (!$this->closed !== false) return false;
-
-		return parent::onUpdate($tick);
-		$hasUpdate = parent::onUpdate($tick);
-
-		if($this->motionX ** 2 + $this->motionZ ** 2 <= $this->moveDirection->lengthSquared()){
-			$motionY = $this->getVelY(); //僵尸运动计算
-			if($motionY >= 0){
-				$this->motionX = $this->moveDirection->x * $this->speed;
-				$this->motionZ = $this->moveDirection->z * $this->speed;
-				$this->motionY = $motionY;
-			}else{
-				$this->moveDirection = $this->generateRandomDirection(); //生成随机运动方向
-				$this->moveTicker = 0;
-				$this->tempTicking = true;
-			}
-		}
-		return $hasUpdate or !$this->onGround or abs($this->motionX) > 0.00001 or abs($this->motionY) > 0.00001 or abs($this->motionZ) > 0.00001;
 	}
 	
 	public function getDrops(){
