3316fdc7f9a6b6f80502381fa83613fbd3c2da17
XinYueNeko <3649745663@qq.com>
2026-07-23 08:16:49 +0800

    fix

--- src/pocketmine/entity/Shulker.php
+++ src/pocketmine/entity/Shulker.php
@@ -1,7 +1,26 @@
 <?php
 namespace pocketmine\entity;
-
+/*
+ * 
+ *  ____                     _                 _             _    ___     ___  
+ * |  _ \   _ __    ___     (_)   ___    ___  | |_          / |  / _ \   / _ \ 
+ * | |_) | | '__|  / _ \    | |  / _ \  / __| | __|  _____  | | | | | | | | | |
+ * |  __/  | |    | (_) |   | | |  __/ | (__  | |_  |_____| | | | |_| | | |_| |
+ * |_|     |_|     \___/   _/ |  \___|  \___|  \__|         |_|  \___/   \___/ 
+ *                        |__/                                                 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author MaoMaoPE Team
+ * @link https://github.com/MaoMaoPE/Project-100
+ *
+ * 
+*/
 //潜影贝
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
 
@@ -9,12 +28,17 @@ class Shulker extends Monster {
     const NETWORK_ID = 54;
 
     public $width = 1.0;
+    public $length = 1.0;
     public $height = 1.0;
 
     public $dropExp = [5, 5];
 
     public function getName(): string {
         return "Shulker";
+    }
+
+    public function __construct(Level $level, CompoundTag $nbt) {
+        parent::__construct($level, $nbt);
     }
 
     public function initEntity(){
