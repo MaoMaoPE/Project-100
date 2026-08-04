ccfc86a239d6d4c44cfcca690a86233d89b6f301
XinYueNeko <3649745663@qq.com>
2026-08-04 10:17:36 +0800

    添加了对pack_manifest.json老式包的支持

--- src/pocketmine/resourcepacks/ZippedResourcePack.php
+++ src/pocketmine/resourcepacks/ZippedResourcePack.php
@@ -71,10 +71,12 @@ class ZippedResourcePack implements ResourcePack{
 
 		$archive = new \ZipArchive();
 		if(($openResult = $archive->open($zipPath)) !== true){
-			throw new \InvalidStateException("打开 $zipPath时遇到ZipArchive错误 $openResult");
+			throw new \InvalidStateException("打开 $zipPath 时遇到ZipArchive错误 $openResult");
 		}
 
 		if(($manifestData = $archive->getFromName("manifest.json")) === false){
+			throw new \InvalidStateException("无法加载材质包 $zipPath: 找不到主类");
+		} elseif (($manifestData = $archive->getFromName("pack_manifest.json")) === false) { //老式的Pack_Manifest.json格式
 			throw new \InvalidStateException("无法加载材质包 $zipPath: 找不到主类");
 		}
 
